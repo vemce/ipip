d8a07182c18327e0d402c565f1686de1ad6fc596
yanmaochun <yanmaochun@ijunchuang.com>
2018-09-07 17:56:43 +0800

    '111'

---
+++ src/Ip.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mc
+ * Date: 2018/9/7
+ * Time: 16:24
+ */
+
+namespace vemce\ipip;
+
+
+use ipip\db\Reader;
+
+class Ip
+{
+    static public $db = null;
+
+    public static function find($ip = '')
+    {
+        self::setDb();
+        return self::$db->find($ip);
+    }
+
+    public static function locationString($ip = '')
+    {
+        $location = self::$db->find($ip);
+        return implode('', array_unique($location));
+    }
+
+    static private function setDb(){
+        if (!self::$db) {
+            $path = __DIR__ . '/ipip.ipdb';
+            self::$db = new Reader($path);
+        }
+    }
+}
